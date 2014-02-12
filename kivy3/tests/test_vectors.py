e9516c748ada0c620021e15edb999aacf5c648e2
Niko Skrypnik <nskrypnik@gmail.com>
2014-02-11 23:32:53 -0800

    cover more cases with testes

--- kivy3/tests/test_vectors.py
+++ kivy3/tests/test_vectors.py
@@ -33,6 +33,29 @@ class Vector3Test(unittest.TestCase):
         v1.add(v2)
         self.assertEqual(v1, [5, 7, 9])
         self.assertEqual(v + 2, [7, 9, 11])
+        
+    def test_sub(self):
+        v1 = Vector3(4, 5, 6)
+        v2 = Vector3(1, 2, 3)
+        v = v1 - v2
+        self.assertEqual(v, [3, 3, 3])
+        v1.sub(v2)
+        self.assertEqual(v1, [3, 3, 3])
+        self.assertEqual(v - 3, [0, 0, 0])
+    
+    def test_multiply(self):
+        v1 = Vector3(5, 6, 7)
+        v2 = Vector3(2, 2, 2)
+        self.assertEqual(v1*v2, [10., 12., 14.])
+        v1.multiply(v2)
+        self.assertEqual(v1, [10., 12., 14.])
+        
+    def test_divide(self):
+        v1 = Vector3(6, 4, 8)
+        v2 = Vector3(2, 2, 2)
+        self.assertEqual(v1 / v2, [3., 2., 4.])
+        v1.divide(v2)
+        self.assertEqual(v1, [3., 2., 4.])
 
 
 if __name__ == '__main__':
