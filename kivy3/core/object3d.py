f5e4fcf46d89e662e8a42af1f91672998d40841a
Niko Skrypnik <niko.skrypnik@cyaninc.com>
2014-03-01 14:54:55 -0800

    Further development of Object3D and providing materials

--- kivy3/core/object3d.py
+++ kivy3/core/object3d.py
@@ -52,20 +52,20 @@ class Object3D(EventDispatcher):
         self.parent = None
 
         self._position = Vector3(0, 0, 0)
+        self._rotation = Vector3(0, 0, 0)
         self._position.set_change_cb(self.on_pos_changed)
-        #self._pop_matrix = PopMatrix()
-        #self._push_matrix = PushMatrix()
-        #self._translate = Translate(*self.pos)
-        #self._scale = Scale(self.scale)
-        #self._rotate_x = Rotate(self.angle_x, 1, 0, 0)
-        #self._rotate_y = Rotate(self.angle_y, 0, 1, 0)
-        #self._rotate_z = Rotate(self.angle_y, 0, 0, 1)
+        self._rotation.set_change_cb(self.on_angle_change)
 
-        mesh_data = kw.pop('mesh', None)
-        if mesh_data:
-            self.set_mesh(mesh_data)
-        else:
-            self.mesh = None
+        # general instructions
+        self._pop_matrix = PopMatrix()
+        self._push_matrix = PushMatrix()
+        self._translate = Translate(*self.pos)
+        self._scale = Scale(self.scale)
+        self._rotors = {
+                        "x": Rotate(self._rotate[0], 1, 0, 0),
+                        "y": Rotate(self._rotate[1], 0, 1, 0),
+                        "y": Rotate(self._rotate[2], 0, 0, 1)
+                        }
 
         self._instructions = None
 
@@ -86,39 +86,51 @@ class Object3D(EventDispatcher):
     position = AliasProperty(_get_position, _set_position)
     pos = position  # just shortcut
 
+    def _set_rotation(self, val):
+        if isinstance(val, Vector3):
+            self._rotation = val
+        else:
+            self._rotation = Vector3(val)
+        self._rotation.set_change_cb(self.on_angle_change)
+        self._rotor["x"].angle = self._rotation.x
+        self._rotor["y"].angle = self._rotation.y
+        self._rotor["z"].angle = self._rotation.z
+
+    def _get_rotation(self):
+        return self._rotation
+
+    rotation = AliasProperty(_get_rotation, _set_rotation)
+
     def on_pos_changed(self, coord, v):
         " Some coordinate was changed "
 
-    def on_angle_x(self, inst, val):
-        self._rotate_x.angle = val
-
-    def on_angle_y(self, inst, val):
-        self._rotate_y.angle = val
-
-    def on_angle_z(self, inst, val):
-        self._rotate_z.angle = val
+    def on_angle_change(self, axis, angle):
+        self._rotor[axis].angle = angle
 
     def on_scale(self, val):
         self._scale.xyz = (val, val, val)
 
-    def set_mesh(self, mesh):
-        if isinstance(mesh, Mesh):
-            self.mesh = mesh
-        else:
-            # normally it should be RawMeshData
-            self.mesh = Mesh(
-                vertices=mesh.vertices,
-                indices=mesh.indices,
-                fmt=mesh.vertex_format,
-                mode='triangles',
-                #texture=texture,
-            )
-
-
     def as_instructions(self):
         """ Get instructions set for renderer """
         if not self._instructions:
-            self._instructions = [self._push_matrix, self._translate, self._rotate_x,
-                                  self._rotate_y, self._rotate_z, self._scale, self.mesh]
+            self._instructions = [self._push_matrix, self._translate,
+                                  self.scale]
+            for rot in self._rotors.itervalues():
+                self._instructions.appned(rot)
+            self._instructions.extend(self.custom_instructions())
+            self._instructions.extend(self.get_children_instructions())
+            self._instructions.append(self._push_matrix)
         return self._instructions
+
+    def custom_instructions(self):
+        """ Should be overriden in subclasses to provide some extra
+            instructions
+        """
+        return []
+
+    def get_children_instructions(self):
+        chilren_instructions = []
+        for child in self.children:
+            chilren_instructions.extend(child.as_instructions())
+        return chilren_instructions
 
--- kivy3/default.glsl
+++ kivy3/default.glsl
@@ -9,15 +9,20 @@ attribute vec2  v_tc0;
 
 uniform mat4 modelview_mat;
 uniform mat4 projection_mat;
+uniform float Tr;
 
 varying vec4 frag_color;
 varying vec2 uv_vec;
+varying vec4 normal_vec;
+varying vec4 vertex_pos;
 
 void main (void) {
     vec4 pos = modelview_mat * vec4(v_pos,1.0);
-    gl_Position = projection_mat * pos;
     frag_color = v_color;
     uv_vec = v_tc0;
+    vertex_pos = pos;
+    normal_vec = vec4(v_normal,0.0);
+    gl_Position = projection_mat * pos;
 }
 
 
@@ -26,12 +31,30 @@ void main (void) {
     precision highp float;
 #endif
 
+varying vec4 normal_vec;
+varying vec4 vertex_pos;
 varying vec4 frag_color;
 varying vec2 uv_vec;
+
+uniform mat4 normal_mat;
+uniform vec3 Kd;
+uniform vec3 Ka;
+uniform vec3 Ks;
+uniform float Tr;
+uniform float Ns;
+uniform float intensity;
+
 
 uniform sampler2D tex;
 
 void main (void){
-    vec4 color = texture2D(tex, uv_vec);
-    gl_FragColor = color;
+    vec4 v_normal = normalize( normal_mat * normal_vec );
+    vec4 v_light = normalize( vec4(0,0,0,1) - vertex_pos );
+    
+    vec3 Id = intensity * Kd;
+    vec3 Ia = intensity * Ka * max(dot(v_light, v_normal), 0.0);
+    vec3 Is = intensity * Ks * pow(max(dot(v_light, v_normal), 0.0), Ns);
+
+    vec4 tex_color = texture2D(tex, uv_vec);
+    gl_FragColor = vec4(Ia + Id*vec3(tex_color) + Is, Tr);
 }
