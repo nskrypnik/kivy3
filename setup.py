6f9e9a4a518e9efe1daddee90e1174c73a7b2a3d
Niko Skrypnik <nskrypnik@gmail.com>
2016-11-20 17:47:02 -0800

    Fixes to setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from setuptools import find_packages, setup
 
-setup(name='Kivy3',
+setup(name='kivy3',
       version='0.1',
       description='Kivy extensions for 3D graphics',
       author='Niko Skrypnik',
       author_email='nskrypnik@gmail.com',
-      packages=find_packages(),
+      packages=find_packages(exclude=("tests",)),
       requires=['kivy', ]
     )
